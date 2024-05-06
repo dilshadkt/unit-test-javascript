ff236f2795313b6dc7fe00a306061aac9aaba1e7
Dilshad <hmydilshadkt@gmail.com>
2024-05-06 14:29:38 +0530

    test 2 fizBuzz

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect } from "vitest";
-import { max } from "../src/intro";
+import { fizzBuzz, max } from "../src/intro";
 
 describe("max", () => {
   it("should return first arguments if it is higher", () => {
@@ -19,5 +19,17 @@ describe("max", () => {
     expect(max(1, 2)).toBe(2));
   it("should be first argument if both arguments are eqaul", () => {
     expect(max(2, 2)).toBe(2);
+  });
+});
+
+describe("fizBuzz", () => {
+  it("it should return FizzBuzz if it is divisible by 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+  it("it should return Fizz if it  is divisible by 3 ", () => {
+    expect(fizzBuzz(3)).toBe("Fizz");
+  });
+  it("should return Buzz if it is divisible by 5 ", () => {
+    expect(fizzBuzz(5)).toBe("Buzz");
   });
 });
